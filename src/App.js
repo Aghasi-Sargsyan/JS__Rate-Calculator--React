f8cb219db3df3e43cf065b8225edfb945dac7f44
Aghasi-Sargsyan <Aghasionline@gmail.com>
2018-10-26 04:03:17 +0400

    app created

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Calculator from "./components/calculator/Calculator";
+import BitcoinRate from "./components/bitcoinRate/BitcoinRate";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            isBcRateVisible: false,
+        };
+
+        this.showBcRate = this.showBcRate.bind(this)
+
+    }
+
+    showBcRate() {
+        this.setState({
+            isBcRateVisible: !this.state.isBcRateVisible
+        });
+    }
+
+    changeBtnTxt() {
+        return this.state.isBcRateVisible ?
+            "Hide Bitcoin rate" :
+            "Show Bitcoin rate";
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Rate calculator</h1>
+                <Calculator/>
+                <button
+                    onClick={this.showBcRate}
+                    className="App__show-hide-btn"
+                    type="button">
+                    {this.changeBtnTxt()}
+                </button>
+                {this.state.isBcRateVisible && <BitcoinRate/>}
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/calculator/Calculator.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import "./Calculator.css";
+
+export default class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.loader = <div className="loader"/>;
+
+        this.state = {
+            currency: "USD",
+            exchangeRate: this.loader,
+            inputValue: ""
+        };
+
+        this.selectChangeHandler = this.selectChangeHandler.bind(this);
+        this.inputChangeHandler = this.inputChangeHandler.bind(this);
+        this.rendSum = this.rendSum.bind(this);
+    }
+
+    selectChangeHandler(e) {
+        let value = e.target.value;
+        this.setState({
+            currency: value,
+            exchangeRate: this.loader
+        });
+        this.fetchExchangeRate(value)
+            .then(data => {
+                this.setState({exchangeRate: Object.values(data)[0]})
+            })
+    }
+
+    inputChangeHandler(e) {
+        this.setState({inputValue: e.target.value});
+    }
+
+    rendSum() {
+        if ((this.state.inputValue / this.state.exchangeRate) > 0) {
+            return (this.state.inputValue / this.state.exchangeRate).toFixed(3)
+        }
+        return <div className="loader loader--sum"/>;
+    }
+
+    componentDidMount() {
+        this.fetchExchangeRate(this.state.currency)
+            .then(data => {
+                this.setState({exchangeRate: Object.values(data)[0]})
+            })
+    }
+
+    fetchExchangeRate(currency) {
+        let path = "http://cb.am/latest.json.php?currency=" + currency;
+        return fetch(path).then(response => response.json());
+    }
+
+
+    render() {
+        return (
+            <div>
+                <div className="select-block">
+                    <span className="select-block__title">Convert AMD to:</span>
+                    <div className="select-block__choose-block">
+                        <select onChange={this.selectChangeHandler}
+                                className="select-block__selector"
+                                id="select-block__selector">
+                            <option>USD</option>
+                            <option>EUR</option>
+                            <option>RUB</option>
+                            <option>GBP</option>
+                        </select>
+                        <span
+                            className="select-block__calc"> (1 {`${this.state.currency}`} = {this.state.exchangeRate} AMD)</span>
+                    </div>
+                </div>
+                <div className="input-block">
+                    <div className="input-block__container">
+                        <input type="number"
+                               className="input-block__input"
+                               id="input-block__input"
+                               placeholder="enter AMD amount"
+                               onChange={this.inputChangeHandler}/>
+                    </div>
+                    <span className="input-block__sum">AMD = {this.rendSum()} {this.state.currency}</span>
+                </div>
+            </div>
+        );
+    }
+
+
+}
